3ce4ba2a742d6472d56e8e08ec967fc982d025ef
Grimille <mickael.dhainaut@gmail.com>
2021-06-20 12:40:59 +0200

    🍱  Added font

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: 'Poppins', sans-serif;
+    font-size: 20px;
+  }
 </style>
